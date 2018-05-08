41308d867501f47499f8f7e334a93c9f6ca2ca91
zayrtun <zay.t@hotmail.com>
2018-05-08 16:20:49 +0630

    0604

--- oop_scatch.php
+++ oop_scatch.php
@@ -1,22 +1,87 @@
 <?php
 
-  class User {
-    private $is_admin = false;
+  // class User {
+  //   private $is_admin = false;
 
-    private function has_admin_access() {
-      return $this->is_admin === true;
-    }
+  //   private function has_admin_access() {
+  //     return $this->is_admin === true;
+  //   }
 
-    public function access_level() {
-      return $this->has_admin_access() ? 'Admin' : 'Standard';
+  //   public function access_level() {
+  //     return $this->has_admin_access() ? 'Admin' : 'Standard';
+  //   }
+  // }
+
+
+  // $user = new User;
+  // // echo $user->is_admin;
+  // //$user->is_admin = true;
+  // // echo $user->has_admin_access();
+  // echo $user->access_level();
+
+
+  // magic method __construct
+  class Product {
+    private $name;
+    private $color;
+
+    public function __construct($name, $color) {
+      $this->name = $name;
+      $this->color = $color;
+      // echo $this->name . "<br>";
+      // echo $this->color . "<br>";
     }
   }
 
+  $shirt = new Product('Jacket', 'SkyBlue');
 
-  $user = new User;
-  // echo $user->is_admin;
-  //$user->is_admin = true;
-  // echo $user->has_admin_access();
-  echo $user->access_level();
+  echo $shirt->name . "<br>";
+  echo $shirt->color . "<br>";
+
+
+    // class Sofa {
+    //   public static $instance_count = 0;
+
+    //   public $seats = 3;
+    //   public $arms = 2;
+
+    //   public function __construct() {
+    //     self::$instance_count++;
+    //   }
+    // }
+
+    // class Couch extends Sofa {
+    //   var $arms = 0;
+    // }
+
+    // class Loveseat extends Sofa {
+    //   var $seats = 2;
+    // }
+
+    // $sofa = new Sofa;
+
+    // class Product {
+    //   public $name;
+    //   public $color;
+
+    //   public function __construct() {
+    //     $this->color = 'blue';
+    //   }
+    // }
+
+    // $shirt = new Product;
+
+    // echo $shirt->color;
+
+
+
+
+
+
+
+
+
+
+
 
 ?>  
